7ec2f67b1b7b1a71b60d78cd1f1f22d1b39f92ff
cang8 <0.gizemgunes@gmail.com>
2023-04-10 09:03:11 +0300

    send test result to server

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import cv2
 import main
+import socket
 
 cap=cv2.VideoCapture(0)
 mycascade = cv2.CascadeClassifier('cascade/cascade.xml')
+
+#connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = ''
+port = 1234
+
+s.connect((host, port))
 
 font1=cv2.FONT_HERSHEY_SIMPLEX
 
@@ -17,8 +25,11 @@ while True:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         img_with_rect = cv2.putText(img = frame,text = 'SHOW TV',org = (x,y),fontFace = font1,fontScale = 1,color = (255,0,0), thickness = cv2.LINE_4)
 
-        if main.pencils.any() :
-            print("Successful")
+        #send "Successful" once to server
+        if main.pencils.any() == True :
+            message = "Successful"
+            s.send(message.encode('utf-8'))
+            s.close()
 
     cv2.imshow("detectionShow",frame)
 
